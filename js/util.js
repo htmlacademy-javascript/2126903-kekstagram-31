751228328ebdbabf6f4807d55eee76a39d511793
Semyon Dudin <semyon.dudin@inbox.ru>
2024-03-16 09:48:30 +0300

    Открывается и закрывается 2 часть

--- js/util.js
+++ js/util.js
@@ -8,4 +8,19 @@ const getRandomInteger = (a, b) => {
 const getRandomArrayElement = (element) => element[getRandomInteger(0, element.length - 1)];
 const isEscKeyDown = (evt) => evt.key === 'Escape';
 
-export { getRandomInteger, getRandomArrayElement, isEscKeyDown };
+const numDecline = (num, nominative, genitiveSingular, genitivePlural) => {
+  const num1 = num % 10;
+  const num2 = num % 100;
+
+  if (num2 === 11 || num2 === 12 || num2 === 13 || num2 === 14) {
+    return (genitivePlural);
+  } else if (num1 >= 2 && num1 <= 4) {
+    return (genitiveSingular);
+  } else if (num1 === 1) {
+    return (nominative);
+  } else {
+    return (genitivePlural);
+  }
+};
+
+export { getRandomInteger, getRandomArrayElement, isEscKeyDown, numDecline };
